ba15da5369404670ecae94705620bf9efd3a50a6
박동연 <qkrehddus01@gmail.com>
2017-11-12 19:28:31 +0900

    문제 발생!! groupmaking 함수가 똑바로 작동하지 않음

--- Main.c
+++ Main.c
@@ -1,167 +1,148 @@
-#include<windows.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <Windows.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-void groupmaking(void);
-void chgroup(void);
-void opengroup(int);
-void add(void);
-void making(int, char);
-void showgroup(void);
-void showcard(int);
-
+#define CLEAN system("cls")
 
 struct group
 {
+	int valid;
+	int cnt; // 카드 개수
+	int Num; // 그룹 번호 (1부터 차곡차곡)
 	char name[20]/*그룹이름*/;
-	char card[101]/*카드번호*/[1001]/*글자수*/;
+	char front_card[101]/*카드번호*/[1001]/*글자수*/;
+	char back_card[101]/*카드번호*/[1001]/*글자수*/;
 };
 
-struct group front[101]/*그룹번호*/;
-struct group back[101];/*그룹번호*/;
+struct group Group[101]/*그룹번호*/;
 
-int main(void)
+void menu();
+void groupmaking(void);
+void opengroup(int num);
+void showgroup(int open);
+void add(void);
+
+// 전역 변수
+int Total_group_NUM = 0;
+
+int main()
 {
-	char c;
+	CLEAN;
+	memset(&Group, 0, sizeof(struct group));
+
+	menu();
+
+	return 0;
+}
+
+void showgroup(int open)
+{
+	printf("\n");
+	for (int i = 0; i < Group[open].cnt; i++) {
+		printf("%d. 앞면 : %s    뒷면 : %s\n",
+			i, Group[open].front_card[i], Group[open].back_card[i]);
+	}
+}
+void add(void)
+{
+	char YN; // yes or no
+	int num;
+	printf("카드를 추가할 그룹번호 : ");
+	scanf("%d", &num);
+
+	if (!Group[num].valid) {
+		printf("어... 해당 그룹번호는 존재하지 않습니다!\n");
+		Sleep(1000);
+		menu();
+	}
+
+	showgroup(num);
+	printf("\n\n");
+
+	while (Group[num].cnt <= 100)
+	{
+		printf("앞면 : ");
+		scanf("%s", Group[num].front_card[Group[num].cnt]);
+		printf("뒷면 : ");
+		scanf("%s", Group[num].back_card[Group[num].cnt]);
+		getchar();
+		printf("-------------------------------\n");
+		printf("카드를 더 추가 하겠습니까?(Y/N)");
+		scanf("%c", &YN);
+		if (YN == 'N' || YN == 'n')
+			break;
+	}
+	if (Group[num].cnt > 100) {
+		printf("한 그룹 당 최대 100개의 카드만 저장 가능합니다!\n");
+		Sleep(1000);
+	}
+	menu();
+}
+void opengroup(int num)
+{
+	CLEAN;
+	if (Group[num].cnt == 0) {
+		printf("흠... 아무것도 없네요?\n");
+		Sleep(1000);
+		menu();
+	}
+	printf("그룹 %s(%d)\n", Group[num].name, Group[num].Num);
+	showgroup(num);
+}
+
+
+void menu(void)
+{
+	CLEAN;
+	char Menu_input;
 
 	printf("press  m / o / a \n");
-	printf("m = 그룹생성 o =  그룹열기 a = 추가");
-	c = getchar();
-	switch (c)
+	printf("m = 그룹생성 o =  그룹열기 a = 추가\n\n");
+	printf("입력해주세요 : ");
+	scanf("%1c", &Menu_input);
+	int open_num;
+	switch (Menu_input)
 	{
 	case 'm':
-	case 'M': groupmaking(); break;
+	case 'M': groupmaking();
+		break;
 
 	case 'o':
-	case 'O': chgroup(); break;
+	case 'O': printf("몇 번을 여시겠습니까?");
+		scanf("%d", &open_num);
+		opengroup(open_num);
+		break;
 
 	case 'a':
-	case 'A': add(); break;
+	case 'A': add();
+		break;
+	default:
+		menu();
+		break;
 	}
 }
 
 void groupmaking(void)
 {
-	int num;
-	char n[20];
+	char Name[20];
 	char YN;
-	int i = 0; // 11월12일 11:46 AM 바꿈 int i; 에서 int i = 0으로.
-	system("cls");
-	printf("그룹을 생성하겠습니까? (Y/N)\n");
-	YN = getchar();
-	system("cls");
+	CLEAN;
+	printf("그룹을 생성하겠습니까? (Y/N) : ");
+	scanf("%1c", &YN);
 
 	if (YN == 'y' || YN == 'Y')
 	{
-		showgroup();
+		printf("생성할 그룹 이름 : ");
+		scanf("%s", Name);
+
+		strcpy(Group[Total_group_NUM].name, Name);
+		Group[Total_group_NUM].Num = Total_group_NUM + 1;
+		Group[Total_group_NUM].valid = 1;
+		printf("그룹 번호는 %d입니다.\n", Total_group_NUM);
+		Total_group_NUM++;
 	}
-
-	if (YN == 'n' || YN == 'N')
-	{
-		main();
-	}
-
-	printf("\n\n");
-	printf("생성할 그룹 번호 : ");
-	scanf_s("%d", &num);
-	printf("생성할 그룹 이름 : ");
-	gets_s(n, sizeof(n));
-	making(num, *n);
-}
-
-void making(int num, char name[])
-{
-	strcpy_s(front[num].name, sizeof(name), name);
-	groupmaking();
-}
-
-void chgroup(void)
-{
-	int i;
-	char a;
-	printf("그룹번호		그룹이름\n");
-	showgroup();
-	printf("\n\n");
-	printf("선택할 그룹의 번호");
-	scanf_s("%c", &a, sizeof(a));
-	opengroup(a);
-}
-
-void opengroup(int num)
-{
-	system("cls");
-	int i;
-	char a;
-	if (front[num].name == NULL)
-	{
-		printf("선택하신 그룹은 존재하지 않습니다.\n");
-		printf("메인화면으로 돌아갑니다.");
-		Sleep(2400);
-		main();
-	}
-	printf("%d번 그룹 | %s", num, front[num].name);
-	printf("카드번호	 front	back");
-	for (i = 1; front[num].card[i][0] != NULL; i++)
-	{
-		printf("%2d번: %s %s", i, front[num].card[i], back[num].card[i]);
-	}
-	printf("\n\n");
-	printf("------------------------------------------------------");
-	printf("main창으로 돌아가기 (m)");
-	while (1)
-	{
-		a = getchar();
-		if (a == 'm')
-			main();
-	}
-}
-
-void add(void)
-{
-	int num;
-	int a;
-	char YN = 'y';
-	system("cls");
-	showgroup();
-	printf("\n\n");
-	printf("카드를 추가할 그룹번호 : ");
-	scanf_s("%d", &num);
-	system("cls");
-	showcard(num);
-	printf("\n\n");
-
-	for (a = 1; YN == 'y'; a++)
-	{
-		printf("front : ");
-		gets_s(front[num].card[a], sizeof(front[num].card[a]));
-		printf("back : ");
-		gets_s(back[num].card[a], sizeof(back[num].card[a]));
-		printf("-------------------------------\n");
-		printf("카드를 더 추가 하겠습니까?(Y/N)");
-		YN = getchar();
-	}
-	main();
-}
-
-void showgroup(void)
-{
-	int i;
-	printf("그룹번호		그룹이름\n");
-	for (i = 1; i <= 100; i++)
-	{
-		if (front[i].name == 0)
-			printf("%d		%s\n", i, front[i].name);
-	}
-}
-
-void showcard(int num)
-{
-	int i;
-	for (i = 1; i <= 100; i++)
-	{
-		if (front[i].name == 0)
-			printf("%d. fornt :	%s   back : %s\n", i, front[num].card[i], back[num].card[i]);
-	}
-
+	else if (YN == 'n' || YN == 'N')
+		menu();
 }
