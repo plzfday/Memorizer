d80a357cab0d109705a415e86d49f22bed705818
박동연 <qkrehddus01@gmail.com>
2017-11-25 17:04:01 +0900

    FINAL

--- Main.c
+++ Main.c
@@ -2,26 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 #include <Windows.h>
-#include <stdbool.h>
+#include <stdbool.h>	
 
 #define CLEAN system("cls")
 
 struct group
 {
-	int valid;
-	int cnt; // 카드 개수
-	char name[20]/*그룹이름*/;
-	char front_card[101]/*카드번호*/[1001]/*글자수*/;
-	char back_card[101]/*카드번호*/[1001]/*글자수*/;
+	int valid;	// ??? 
+	int cnt;	// 카드 개수
+	char name[20];	//그룹 이름
+	char front_card[101][1001];	// 카드 개수, 글자 수
+	char back_card[101][1001];	// 카드 번호, 글자 수
 };
 
-/*
-	ToDoList
-	0. 변수 저장 시스템의 문제점.
-	1. 공부 시스템 - 현재의 시스템의 문제점 : 공부 불가능.
-*/
+struct again
+{
+	int valid;
+	int cnt;
+	char name[20];
+	char front_card[101][1001];
+	char back_card[101][1001];
+};
 
-struct group Group[101]/*그룹번호*/;
+struct copy
+{
+	int valid;
+	int cnt;
+	char name[20];
+	char front_card[101][1001];
+	char back_card[101][1001];
+};
+
+struct group Group[101];	// 그룹 이름
+struct again Again[101];	// 다시 반복할 그룹만 따로 만듦 (매번 초기화 해줘야 함)
+struct copy Copy[101];
 
 void menu();
 void groupmaking(void);
@@ -29,6 +43,7 @@ void groupmaking(void);
 void opengroup(int num);
 void showgroup(int open);
 void add(void);
+void description(void);
 
 // 전역 변수
 int Total_group_NUM = 0;
@@ -36,22 +51,29 @@ int Total_group_NUM = 0;
 int main()
 {
 	CLEAN;
+	system("mode con lines=25 cols=65");
+	SetConsoleTitle(TEXT("성훈이 버스 부르릉~!"));
 	memset(&Group, 0, sizeof(struct group));
+	memset(&Again, 0, sizeof(struct again));
+	memset(&Copy, 0, sizeof(struct copy));
 	menu();
+
 
 	return 0;
 }
 
+
+// 메뉴----------------------- 게임 시작
 void menu(void)
 {
 	CLEAN;
 	char Menu_input;
 
-	printf("press  m / o / a\n");
-	printf("m = 그룹생성 o =  그룹열기 a = 추가 / 나머지 = 종료\n\n");
+	printf("press  m / o / a\n\n");
+	printf("m = 그룹생성 \n\no =  그룹열기(공부하기) \n\na = 카드추가 \n\nh = 도움말\n\n나머지 = 종료\n\n");
 	printf("입력해주세요 : ");
 	Menu_input = getchar();
-	getchar();
+	while (getchar() != '\n');
 	int open_num;
 	switch (Menu_input)
 	{
@@ -64,27 +86,35 @@ void menu(void)
 	case 'O':
 		printf("몇 번을 여시겠습니까?");
 		scanf("%d", &open_num);
-		opengroup(open_num);
+		opengroup(open_num);	// 그룹 번호 검사 함수 실행
 		break;
 
 	case 'a':
 	case 'A':
 		add();
 		break;
+	case 'h':
+	case 'H':
+		description();
+		break;
 	default:
 		exit(1);
 		break;
 	}
 }
+// 메뉴 끝 
 
+
+
+// 그룹 만들기--------------------------------
 void groupmaking(void)
 {
 	CLEAN;
 	char YN;
 	char Name[20];
-	printf("그룹을 생성하겠습니까? (Y/N) \n나머지 : 종료\n\n입력 : ");
+	printf("그룹을 생성하겠습니까? (Y/N) \n나머지 : 뒤로\n\n입력 : ");
 	YN = getchar();
-	getchar();
+	while (getchar() != '\n');
 	if (YN == 'y' || YN == 'Y')
 	{
 		printf("그룹 이름을 정해주세요.\n");
@@ -92,31 +122,104 @@ void groupmaking(void)
 
 		Group[Total_group_NUM].valid = 1;
 		printf("그룹 번호는 %d입니다.\n", Total_group_NUM);
-		Sleep(1000);
+		while (getchar() != '\n');
 		Total_group_NUM++;
 		menu();
 	}
-	else if (YN == 'n' || YN == 'N')
+	else {
 		menu();
-	else
-		exit(1);
+	}
 }
+// 그룹 만들기 끝
 
+
+
+
+// 그룹 보여주기----------------------------- 공부 시작
 void showgroup(int open)
 {
+	int NM;
+	int j = 0;
+	int i, h;
+
+	getchar();
 	printf("\n");
-	for (int i = 0; i < Group[open].cnt; i++) {
-		printf("%d. 앞면 : %s    뒷면 : %s\n",
-			i + 1, Group[open].front_card[i], Group[open].back_card[i]);
+	for (i = 0; i < Group[open].cnt; i++) {
+		printf("%d. 앞면 : %s\n", i + 1, Group[open].front_card[i]);
+		getchar();
+		while (getchar() != '\n');
+		printf("%d. 뒷면 : %s\n", i + 1, Group[open].back_card[i]);
+		printf("기억남 = n / 기억안남 = m\n입력 : ");
+		NM = getchar();
+		CLEAN;
+		if (NM == 'n' || NM == 'N') {
+
+		}
+		else if (NM == 'm' || NM == 'M') {
+			strcpy(Again[open].front_card[j], Group[open].front_card[i]);	// 기억이 나지 않는 카드만 Again 에 저장
+			strcpy(Again[open].back_card[j], Group[open].back_card[i]);
+			Again[open].cnt++;
+			j++;
+		}
 	}
+	i = 0;
+	j = 0;
+	h = 0;
+	while (Again[open].cnt > 0) {
+
+		for (i = 0; i < Again[open].cnt; i++) {
+			printf("%d. 앞면 : %s\n", i + 1, Again[open].front_card[i]);
+			getchar();
+			while (getchar() != '\n');
+			printf("%d. 뒷면 : %s\n", i + 1, Again[open].back_card[i]);
+			printf("기억남 = n / 기억안남 = m\n입력 : ");
+			NM = getchar();
+			CLEAN;
+			if (NM == 'n' || NM == 'N') {
+				Again[open].cnt--;
+			}
+			else if (NM == 'm' || NM == 'M') {
+				strcpy(Copy[open].front_card[j], Again[open].front_card[i]);	// 기억이 나지 않는 카드만 copy 에 저장
+				strcpy(Copy[open].back_card[j], Again[open].back_card[i]);
+				Copy[open].cnt++;
+				j++;
+			}
+		}
+
+		for (i = 0; i < Copy[open].cnt; i++) {
+			printf("%d. 앞면 : %s\n", i + 1, Copy[open].front_card[i]);
+			getchar();
+			while (getchar() != '\n');
+			printf("%d. 뒷면 : %s\n", i + 1, Copy[open].back_card[i]);
+			printf("기억남 = n / 기억안남 = m\n입력 : ");
+			NM = getchar();
+			CLEAN;
+			if (NM == 'n' || NM == 'N') {
+				Copy[open].cnt--;
+			}
+			else if (NM == 'm' || NM == 'M') {
+				strcpy(Again[open].front_card[j], Copy[open].front_card[i]);	// 기억이 나지 않는 카드만 copy 에 저장
+				strcpy(Again[open].back_card[j], Copy[open].back_card[i]);
+				Again[open].cnt++;
+				j++;
+			}
+		}
+	}
+
+
 	char chk = NULL;
 	printf("되돌아 가시겠습니까? (Press any) ");
 	scanf("%c", &chk);
-	getchar();
+	while (getchar() != '\n');
 	if (chk != NULL)
 		menu();
 }
+// 그룹 보여주기 끝
 
+
+
+
+// 카드 추가----------------------------- 카드 생성 시작
 void add(void)
 {
 	char YN; // yes or no
@@ -124,13 +227,13 @@ void add(void)
 	printf("카드를 추가할 그룹번호 : ");
 	scanf("%d%*c", &num);
 	if (num > 100 || num < 0) {
-		printf("그룹 번호 최대는 100입니다.\n도움말을 참고하세요.\n");
-		Sleep(1000);
+		printf("그룹 번호 0에서 100까지 입니다.\n도움말을 참고하세요.\n");
+		while (getchar() != '\n');
 		menu();
 	}
 	if (!Group[num].valid) {
-		printf("어... 해당 그룹번호는 존재하지 않습니다!\n");
-		Sleep(1000);
+		printf("해당 그룹번호는 존재하지 않습니다!\n");
+		while (getchar() != '\n');
 		menu();
 	}
 
@@ -148,9 +251,9 @@ void add(void)
 		printf("-------------------------------\n");
 		printf("카드를 더 추가 하겠습니까?(Y/N)");
 
-		
+
 		scanf("%c", &YN);
-		getchar();
+		while (getchar() != '\n');
 
 		/*printf("[debug]YN : %c\n", YN);*/
 
@@ -159,7 +262,7 @@ void add(void)
 		{
 			if (Group[num].cnt > 100) {
 				printf("한 그룹 당 최대 100개의 카드만 저장 가능합니다!\n");
-				Sleep(1000);
+				while (getchar() != '\n');
 				menu();
 			}
 		}
@@ -168,7 +271,11 @@ void add(void)
 		}
 	}
 }
+// 카드 추가 끝
 
+
+
+// 오픈그룹-------------------	그룹 번호 검사함 (번호가 이상하거나 존재하지 않으면 오류 발생시켜줌)
 void opengroup(int num)
 {
 	CLEAN;
@@ -182,3 +289,29 @@ void opengroup(int num)
 	printf("그룹 : %s(%d)\n", Group[num].name, num);
 	showgroup(num);
 }
+// 오픈그룹 끝
+
+
+// 메뉴얼
+void description(void)
+{
+	CLEAN;
+	fputs("안녕하세요! 성훈이 버스 부르릉입니다.\n", stdout);
+	Sleep(1000);
+	fputs("간단하게 설명해드리겠습니다.\n\n", stdout);
+	Sleep(1000);
+	fputs("m(그룹생성):그룹을 생성합니다.\na(카드추가):그룹에 카드를 추가합니다.\no(그룹열기):생성된 그룹을 열 수 있으며 공부를 시작하게 됩니다.\nh(도움말):도움말입니다.\n\n", stdout);
+	char ch = NULL;
+	printf("Continue (Press any) ");
+	scanf("%c", &ch);
+	while (getchar() != '\n');
+	fputs("1. 우선 그룹을 생성하세요. 그룹 번호가 주어집니다.\n2. a를 눌러 해당 그룹에 카드를 추가합니다.(최대 100개)\n3. 공부를 하시려면 o(그룹열기)를 눌러 공부를 시작하시면 됩니다!\n\n감사합니다.\nCopyright(c)2017 성훈이 버스 부르릉 All rights reserved.\n", stdout);
+
+	char chk = NULL;
+	printf("\n\n되돌아 가시겠습니까? (Press any) ");
+	scanf("%c", &chk);
+	while (getchar() != '\n');
+	if (chk != NULL)
+		menu();
+}
+// 메뉴얼 끝
